f5c9983d1fbd26b46baec528fce8ce57e3419e2b
группы стационара <st@itstep.lan>
2019-11-14 19:35:35 +0300

    14.11.2019 commit

---
+++ server.js
@@ -0,0 +1,17 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/angular-blog'));
+
+app.get('/*', function(req,res) {
+    
+    res.sendFile(path.join(__dirname,'/dist/angular-blog/index.html'));
+
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
