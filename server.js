c717ff6170ea1574e4547afbebe0efcf3ab241ac
Pavel Rachkovsky <pavel.rachkovsky@vrpconsulting.com>
2019-11-19 18:00:29 +0300

    added database connection

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const {
+  Client
+} = require('pg');
 
 const app = express();
+
+const client = new Client({
+  connectionString: 'postgres://abzpwypezvpfqb:1c926030fbc5337b8063fdfe25c32bdd885ecc4d4bdf20deb2c5960f397da308@ec2-54-227-249-108.compute-1.amazonaws.com:5432/d14u63hi0e2f4g',
+  ssl: true
+});
+
+client.connect(() => {
+  console.log('Database connected');
+});
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/angular-blog'));
@@ -17,18 +29,17 @@ app.get('/*', function (req, res) {
 app.post('/api/test', function (req, res) {
   // here will be request to DB ....
   res.send(JSON.stringify(
-    [
-      {
-        "userId": 1,
-        "id": 1,
-        "title": "sunt aut facere repellat provident occaecati",
-        "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas"
+    [{
+        userId: 1,
+        id: 1,
+        title: "sunt aut facere repellat provident occaecati",
+        body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas"
       },
       {
-        "userId": 1,
-        "id": 2,
-        "title": "qui est esse",
-        "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis"
+        userId: 1,
+        id: 2,
+        title: "qui est esse",
+        body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis"
       }
     ]
   ));
